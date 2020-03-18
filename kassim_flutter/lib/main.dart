60b614aa3ef6e03892306f13669b083567ca3bdb
Ahmed Ragab <ragaba287@gmail.com>
2020-03-18 04:12:18 +0200

    Splash_screen

--- kassim_flutter/lib/main.dart
+++ kassim_flutter/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/splash.dart';
+import 'package:kassim_flutter/screens/splash.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +11,18 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Kassim',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: MaterialColor(Color(0xff0d0e1a).value, {
+          50: Colors.grey.shade50,
+          100: Colors.grey.shade100,
+          200: Colors.grey.shade200,
+          300: Colors.grey.shade300,
+          400: Colors.grey.shade400,
+          500: Colors.grey.shade500,
+          600: Colors.grey.shade600,
+          700: Colors.grey.shade700,
+          800: Colors.grey.shade800,
+          900: Colors.grey.shade900
+        }),
       ),
       home: SplashScreen(),
     );
--- kassim_flutter/lib/screens/splash.dart
+++ kassim_flutter/lib/screens/splash.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:kassim_flutter/Animations/FadeAnimation.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -34,59 +35,139 @@ class _SplashScreen extends State<SplashScreen> {
     return Scaffold(
       backgroundColor: Color(0xff16172B),
       body: Container(
+        padding: EdgeInsets.all(0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            SizedBox(height: 240),
-            Divider(
-              color: Color(0xffE1CB69),
-              endIndent: 130,
-              indent: 130,
-              thickness: 3,
-            ),
-            Text(
-              'Welcome Dr.Kassim',
-              style: TextStyle(
-                color: Color(0xffE1CB69),
-                letterSpacing: 3,
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(height: 190),
-            CustomElevation(
-              child: FlatButton(
-                //elevation: 1,
-                onPressed: () {},
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0)),
-                padding: EdgeInsets.all(0.0),
-                child: Ink(
-                  decoration: BoxDecoration(
-                    boxShadow: [],
-                    gradient: LinearGradient(
-                      colors: [
-                        Color(0xffFAEDD0),
-                        Color(0xffEAB948),
-                      ],
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                FadeAnimation(
+                  1.5,
+                  -20,
+                  400,
+                  ClipRect(
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      heightFactor: .5,
+                      widthFactor: .5,
+                      child: Container(
+                        height: 150,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color(0xff242537),
+                        ),
+                      ),
                     ),
-                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                  child: Container(
-                    constraints:
-                        BoxConstraints(maxWidth: 200.0, minHeight: 50.0),
-                    alignment: Alignment.center,
-                    child: Text(
-                      'Login',
-                      style: TextStyle(
-                        color: Color(0xff040407),
-                        letterSpacing: 5,
-                        fontSize: 20,
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                FadeAnimation(
+                  1.5,
+                  20,
+                  500,
+                  Container(
+                    height: 180,
+                    child: Image(image: AssetImage('assets/logo.png')),
+                  ),
+                ),
+                SizedBox(height: 10),
+                FadeAnimation(
+                  1.5,
+                  40,
+                  500,
+                  Divider(
+                    color: Color(0xffE1CB69),
+                    endIndent: 130,
+                    indent: 130,
+                    thickness: 4,
+                  ),
+                ),
+                FadeAnimation(
+                  1.6,
+                  80,
+                  500,
+                  Text(
+                    'Welcome Dr.Kassim',
+                    style: TextStyle(
+                      color: Color(0xffE1CB69),
+                      letterSpacing: 1.8,
+                      fontSize: 18,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 80),
+            FadeAnimation(
+              1.5,
+              50,
+              500,
+              CustomElevation(
+                child: FlatButton(
+                  //elevation: 1,
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  padding: EdgeInsets.all(0.0),
+                  child: Ink(
+                    decoration: BoxDecoration(
+                      boxShadow: [],
+                      gradient: LinearGradient(
+                        colors: [
+                          Color(0xffFAEDD0),
+                          Color(0xffEAB948),
+                        ],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Container(
+                      constraints:
+                          BoxConstraints(maxWidth: 240.0, minHeight: 50.0),
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Color(0xff040407),
+                          letterSpacing: 5,
+                          fontSize: 22,
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                FadeAnimation(
+                  1.5,
+                  10,
+                  400,
+                  ClipRect(
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      heightFactor: .5,
+                      widthFactor: .5,
+                      child: Container(
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color(0xff242537),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
